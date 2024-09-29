9838a3aba6bf1a53fd2f8d000519535ee2e5b7a2
Magnus <magnus.egeland@gmail.com>
2024-09-29 17:36:33 +0200

    updated to work with the new services

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WeatherApp.Interfaces;
 
-namespace weatherapp.Pages;
+namespace WeatherApp.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IGeocodingService _geocodingService;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public string Location {get;set;}
+
+
+    public IndexModel(ILogger<IndexModel> logger, IGeocodingService geocodingService)
     {
         _logger = logger;
+        _geocodingService = geocodingService;
     }
 
     public void OnGet()
     {
 
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if(string.IsNullOrEmpty(Location))
+        {
+             // If no location is provided, stay on the page and show an error (optional)
+            ModelState.AddModelError(string.Empty, "Location is required.");
+            return Page();
+        }
+
+        var locationData = await _geocodingService.GetCoordinatesAsync(Location);
+        double latitude = double.Parse(locationData.Lat);
+        double longitude = double.Parse(locationData.Lon);
+
+        return RedirectToPage("WeatherForecast/index", new{Latitude = latitude, Longitude = longitude});
     }
 }
--- Pages/WeatherForecast/index.cshtml.cs
+++ Pages/WeatherForecast/index.cshtml.cs
@@ -2,34 +2,38 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using WeatherApp.Interfaces;
 using WeatherApp.Models;
 
 namespace WeatherApp.Pages.WeatherForecast
 {
     public class WeatherForecastModel : PageModel
     {
-        private readonly IHttpClientFactory _clientfactory;
+        private readonly IWeatherForcastService _weatherForecastSerivce;
+
         public Forecast.Root ForecastData { get; set; }
-        public WeatherForecastModel(IHttpClientFactory clientFactory)
+        public WeatherForecastModel(IWeatherForcastService weatherForecastService)
         {
-            _clientfactory = clientFactory;
+            _weatherForecastSerivce = weatherForecastService;
         }
-        public async Task OnGetAsync()
+
+        [BindProperty(SupportsGet = true)]
+        public double Latitude {get;set;}
+
+        [BindProperty(SupportsGet = true)]
+        public double Longitude {get;set;}
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            string latitude = "60.10";
-            string longitude = "9.58";
-            string url = $"https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={latitude}&lon={longitude}";
+            if (Latitude == 0 || Longitude == 0)
+            {
+                return NotFound("Coordinates not provided");
+            }
+            var forecast = await _weatherForecastSerivce.GetWetherAsync(Latitude, Longitude);
+            ForecastData = forecast;
 
-            var client = _clientfactory.CreateClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "MyWeatherApp");
-
-            HttpResponseMessage response = await client.GetAsync(url);
-
-            string content = await response.Content.ReadAsStringAsync();
-
-            ForecastData = JsonConvert.DeserializeObject<Forecast.Root>(content);
-
-            Console.WriteLine("Done");
+            
+            return Page();
         }
     }
 }
